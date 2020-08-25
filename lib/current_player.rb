0e95ddcf985468710c433938a3df632f7870fe56
Charles Lai <charlesangeloplai@gmail.com>
2020-08-25 21:48:21 +0000

    Automatically backed up by Learn

--- lib/current_player.rb
+++ lib/current_player.rb
@@ -1,8 +1,7 @@
-def turn_count(board){
-  count = 0
-  board.each{
-    |turn|
-    if turn == "X" || turn == "O"
-      count += 1
-  }
-}
+def turn_count(board)
+  counter = 0
+  board.each do |turn_counter|
+    if turn_counter == "X" || turn_counter == "O"
+      counter += 1
+    end
+  end
